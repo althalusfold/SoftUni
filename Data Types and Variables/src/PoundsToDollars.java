0951e4b227f97ea2b3940c994047a4a70ed083cf
althalusfold <althalus90@gmail.com>
2024-01-18 14:28:09 +0200

    Learning

--- Data Types and Variables/src/PoundsToDollars.java
+++ Data Types and Variables/src/PoundsToDollars.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class PoundsToDollars {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        float moneyGBP = scanner.nextFloat();
+        System.out.printf("%.3f", moneyGBP*1.36);
     }
 }
