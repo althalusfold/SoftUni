6a23be15cb2136a823cf1f99d1bfe7b1f52f4d37
althalusfold <althalus90@gmail.com>
2024-02-17 16:38:03 +0200

    Fix

--- Lists/src/BombNumbers.java
+++ Lists/src/BombNumbers.java
@@ -1,13 +1,14 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+
 public class BombNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<Integer> sequence = Arrays.stream(scanner.nextLine()
-                        .split(" "))
+        List<Integer> sequence = Arrays.stream(scanner.nextLine().split(" "))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
 
@@ -15,42 +16,34 @@ public class BombNumbers {
                 .mapToInt(Integer::parseInt)
                 .toArray();
 
-        int sum = 0;
-        int bombNumberIndex = 0;
-        int bombRadius = bombNumbers[1];
-        int start = 0;
-        int end = 0;
+        int sum = detonateBombs(sequence, bombNumbers[0], bombNumbers[1]);
 
-        if(!(bombRadius >= sequence.size()-1) && !(sequence.size() <= bombRadius * 2)) {
-
-            for (int i = 0; i < sequence.size(); i++) {
-
-                if (Objects.equals(sequence.get(i), bombNumbers[0])) {
-                    bombNumberIndex = i;
-
-                    if (bombNumberIndex >= bombRadius) {
-                        start = bombNumberIndex - bombRadius;
-                        end = bombNumberIndex + bombRadius;
-                        for (int j = start; j <= end; j++) {
-                            if (start == sequence.size()) {
-                                break;
-                            }
-                            sequence.remove(start);
-                        }
-                    } else {
-                        for (int j = 0; j <= bombRadius; j++) {
-                            sequence.remove(0);
-                        }
-                    }
-                }
-            }
-        }else{
-            sequence.clear();
-        }
-        for(int number : sequence){
-           sum += number;
-        }
         System.out.println(sum);
     }
-}
 
+    public static int detonateBombs(List<Integer> sequence, int bombNumber, int bombRadius) {
+        int sum = 0;
+
+        for (int i = 0; i < sequence.size(); i++) {
+            if (Objects.equals(sequence.get(i), bombNumber)) {
+                int startIndex = Math.max(0, i - bombRadius);
+                int endIndex = Math.min(sequence.size() - 1, i + bombRadius);
+
+                // Remove elements within the bomb radius
+                for (int j = endIndex; j >= startIndex; j--) {
+                    sequence.remove(j);
+                }
+
+                // Update the loop counter to account for removed elements
+                i = Math.max(0, i - bombRadius - 1);
+            }
+        }
+
+        // Calculate the sum of the remaining elements
+        for (int number : sequence) {
+            sum += number;
+        }
+
+        return sum;
+    }
+}
