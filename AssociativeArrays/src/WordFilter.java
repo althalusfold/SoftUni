c3c64901ba2b204403de631cae8b546dc39c4588
althalusfold <althalus90@gmail.com>
2024-03-07 12:11:18 +0200

    Associative Arrays Training

---
+++ AssociativeArrays/src/WordFilter.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class WordFilter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] words = Arrays.stream(scanner.nextLine().split(" "))
+                .filter(word -> word.length() % 2 == 0)
+                .toArray(String[] :: new);
+
+        //1 начин -> StreamAPI
+        //words = ["kiwi", "orange", "banana"]
+        //Arrays.stream(words).forEach(word -> System.out.println(word));
+
+        //2 начин -> foreach
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
+}
