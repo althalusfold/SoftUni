f593ae5fc04cbff34238962c4a94ffb36a2c95a0
althalusfold <althalus90@gmail.com>
2024-03-18 16:29:34 +0200

    Fix

--- FinalExam/src/WorldTour.java
+++ FinalExam/src/WorldTour.java
@@ -2,13 +2,12 @@ import java.util.Scanner;
 public class WorldTour {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         String allStops = scanner.nextLine();
         StringBuilder stopsBuilder = new StringBuilder(allStops);
-
         String command = scanner.nextLine();
+
         while(!command.equals("Travel")) {
-            //команда
+
             if (command.contains("Add Stop")) {
                 int index = Integer.parseInt(command.split(":")[1]);
                 String stopName = command.split(":")[2];
@@ -18,20 +17,16 @@ public class WorldTour {
                 }
 
             } else if (command.contains("Remove Stop")) {
-
                 int startIndex = Integer.parseInt(command.split(":")[1]);
                 int endIndex = Integer.parseInt(command.split(":")[2]);
-
 
                 if (isValidIndex(startIndex, stopsBuilder) && isValidIndex(endIndex, stopsBuilder)) {
                     stopsBuilder.delete(startIndex, endIndex + 1);
                 }
 
             } else if (command.contains("Switch")) {
-
                 String oldText = command.split(":")[1];
                 String newText = command.split(":")[2];
-
 
                 if (stopsBuilder.toString().contains(oldText)) {
                     String updatedText = stopsBuilder.toString().replace(oldText, newText);
